b807dea538e234a47f4291d3d0ceb476d095a235
Zeeshan <zh012948@gmail.com>
2025-05-24 00:26:31 +0500

    initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path='/login' element={<Login />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/welcome' element={<Welcome />} />
